14caaf2b7b9b8db1019c4e8423115472fd378abd
davidhigginbotham <dhigginbotham@clearent.com>
2019-03-20 08:09:00 -0500

    new framework

--- ClearentIdtechIOSFrameworkPod/ClearentIdtechIOSFrameworkPod/Frameworks/ClearentIdtechIOSFramework.framework/Headers/ClearentIdtechIOSFramework.h
+++ ClearentIdtechIOSFrameworkPod/ClearentIdtechIOSFrameworkPod/Frameworks/ClearentIdtechIOSFramework.framework/Headers/ClearentIdtechIOSFramework.h
@@ -6,22 +6,5 @@
 //  Copyright © 2018 Clearent, L.L.C. All rights reserved.
 //
 #import "ClearentIdtechIOSFramework/Clearent_VP3300.h"
-#import "ClearentIdtechIOSFramework/ClearentManualEntry.h"
-#import <IDTech/IDTech.h>
-#import <IDTech/IDT_Device.h>
-#import <IDTech/IDT_BTPay.h>
-#import <IDTech/IDT_BTMag.h>
-#import <IDTech/IDT_iMag.h>
-#import <IDTech/IDT_UniPay.h>
-#import <IDTech/IDT_UniMag.h>
-#import <IDTech/IDTMSRData.h>
-#import <IDTech/IDTEMVData.h>
-#import <IDTech/IDTUtility.h>
-#import <IDTech/APDUResponse.h>
-#import <iDTech/IDT_VP3300.h>
-#import <iDTech/IDT_NEO2.h>
-#import <iDTech/IDT_UniPayI_V.h>
-#import <iDTech/uniMag.h>
-#import <IDTech/IDTCommon.h>
-
+#import "IDTech/IDTUtility.h"
 
