47ab464a36b5b581b9d7b0399d4abffb2649c73c
davidhigginbotham <dhigginbotham@clearent.com>
2019-03-22 12:30:03 -0500

    remove idtech headers from the umbrealla header

--- ClearentIdtechIOSFrameworkPod/ClearentIdtechIOSFramework.framework/Headers/ClearentIdtechIOSFramework.h
+++ ClearentIdtechIOSFrameworkPod/ClearentIdtechIOSFramework.framework/Headers/ClearentIdtechIOSFramework.h
@@ -10,17 +10,3 @@
 #import "ClearentIdtechIOSFramework/ClearentPublicVP3300Delegate.h"
 #import "ClearentIdtechIOSFramework/ClearentManualEntryDelegate.h"
 #import "ClearentIdtechIOSFramework/ClearentManualEntry.h"
-#import <IDTech/IDT_Device.h>
-#import <IDTech/IDT_BTPay.h>
-#import <IDTech/IDT_BTMag.h>
-#import <IDTech/IDT_iMag.h>
-#import <IDTech/IDT_UniPay.h>
-#import <IDTech/IDT_UniMag.h>
-#import <IDTech/IDTMSRData.h>
-#import <IDTech/IDTEMVData.h>
-#import <IDTech/IDTUtility.h>
-#import <IDTech/APDUResponse.h>
-#import <IDTech/IDT_VP3300.h>
-#import <IDTech/IDT_NEO2.h>
-#import <IDTech/IDT_UniPayI_V.h>
-#import <IDTech/uniMag.h>
