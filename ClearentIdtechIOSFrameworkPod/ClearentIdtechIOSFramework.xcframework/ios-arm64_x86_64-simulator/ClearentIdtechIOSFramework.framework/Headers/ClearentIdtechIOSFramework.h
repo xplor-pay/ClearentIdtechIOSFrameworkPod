8fa59f981bdb9b6666e7aa05ae179a8f335c4ca6
Seth <sarmbruster@clearent.com>
2023-05-12 14:24:47 -0500

    proving i can build this

--- ClearentIdtechIOSFrameworkPod/ClearentIdtechIOSFramework.xcframework/ios-arm64_x86_64-simulator/ClearentIdtechIOSFramework.framework/Headers/ClearentIdtechIOSFramework.h
+++ ClearentIdtechIOSFrameworkPod/ClearentIdtechIOSFramework.xcframework/ios-arm64_x86_64-simulator/ClearentIdtechIOSFramework.framework/Headers/ClearentIdtechIOSFramework.h
@@ -5,7 +5,6 @@
 //  Created by David Higginbotham on 5/3/18.
 //  Copyright © 2018 Clearent, L.L.C. All rights reserved.
 //
-
 #import "Clearent_VP3300.h"
 #import "ClearentManualEntry.h"
 
@@ -25,5 +24,19 @@
 #import "ClearentDeviceConnector.h"
 #import "ClearentIDTUtility.h"
 
-#import <IDTech/IDTech.h>
-
+#import <IDTech/IDT_VP3300.h>
+#import <IDTech/IDTCommon.h>
+#import <IDTech/IDTUtility.h>
+#import <IDTech/IDT_Device.h>
+#import <IDTech/IDT_BTPay.h>
+#import <IDTech/IDT_BTMag.h>
+#import <IDTech/IDT_iMag.h>
+#import <IDTech/IDT_UniPay.h>
+#import <IDTech/IDT_UniMag.h>
+#import <IDTech/IDTMSRData.h>
+#import <IDTech/IDTEMVData.h>
+#import <IDTech/APDUResponse.h>
+#import <IDTech/APDUData.h>
+#import <IDTech/IDT_UniPayI_V.h>
+#import <IDTech/uniMag.h>
+#import <IDTech/UniPay.h>
